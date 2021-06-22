e9f7bcec5704173d01e5d604cff3542a4890717f
unknown <andreykrendzel@hotmail.com>
2021-06-22 08:09:18 +0100

    Functioning application

---
+++ controllers/blogs.js
@@ -0,0 +1,39 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  /* 
+    title: String,
+  author: String,
+  url: String,
+  likes: Number
+  */
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+
+
+module.exports = blogsRouter
